f8db46a58906518c97a0c2c349e01ccaa9551eee
ejiao <edward_jiao@brown.edu>
2016-11-13 10:55:55 -0500

    added event handlers for mouse motion

--- src/widget/Variable.jsx
+++ src/widget/Variable.jsx
@@ -2,33 +2,48 @@ import React from 'react';
 import { connect } from 'react-redux';
 
 import { selectValue } from './store/selectors';
+import { setVariable } from './store/actions';
 
 class Variable extends React.Component {
     constructor() { 
         this.state = {
             status: "inactive";
+            x: null; 
+            y: null; 
         }
     }
-    handleMouseEnter() { // when hovered 
+    handleMouseEnter(event) { // when hovered 
         this.setState({status: "ready"}); 
     } 
-    handleMouseDown() { // 
+    handleMouseDown(event) { // 
         this.setState({status: "active"}); 
+        var e = window.event;
+        this.setState({x: e.clientX}); 
+        this.setState({y: e.clientY}); 
+        
     }
-    handleMouseUp() { // 
+    handleMouseMove(event) { 
+        if (this.state.status == "active") {
+            var newx = event.clientX; 
+            var value = this.props.value + (newx - this.state.x); 
+            this.props.setVariable({value: value}); 
+        }
+    }
+    handleMouseUp(event) { // 
         this.setState({status: "inactive"});
     }
-    handleMouseLeave() { 
+    handleMouseLeave(event) { 
         if (this.state.status == "ready") {
             this.setState({status: "inactive"}); 
         }      
     }
 
     render() {
-        return <span onMouseEnter = {handleMouseEnter()} 
-                onMouseDown = {handleMouseDown()}
-                onMouseUp = {handleMouseUp()}
-                onMouseLeave = {handleMouseLeave()} >
+        return <span onMouseEnter = {handleMouseEnter(event)} 
+                onMouseDown = {handleMouseDown(event)}
+                onMouseUp = {handleMouseUp(event)}
+                onMouseLeave = {handleMouseLeave(event)} 
+                onMouseMove = {handleMouseMove(event)}>
                     { this.props.value }
                 </span>
     }
@@ -41,7 +56,15 @@ function mapStateToProps(state, ownProps) {
     };
 }
 
-Variable = connect(mapStateToProps)(Variable);
+function mapDispatchToProps(dispatch, ownProps) {
+    return {
+        setVariable(value) {
+            dispatch(setVariable(ownProps.widgetId, ownProps.name, value));
+        }
+    }
+}
+
+Variable = connect(mapStateToProps, mapDispatchToProps)(Variable);
 
 export default class VariableWrapper extends React.Component {
     render() {
